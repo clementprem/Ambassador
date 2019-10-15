ce1d07bd9268811b2544c8967f15f679e308cf79
Clement Prem <clementprem@gmail.com>
2019-10-15 17:42:32 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Ambassador",
+     platforms: [
+        .iOS(.v11),
+    ],
     products: [
         .library(name: "Ambassador", targets: ["Ambassador"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:clementprem/Embassy.git", from: "4.1.1")
+        .package(url: "git@github.com:clementprem/Embassy.git", from: "4.1.2")
     ],
     targets: [
         .target(name: "Ambassador", dependencies: ["Embassy"], path: "Ambassador"),
